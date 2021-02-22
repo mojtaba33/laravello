d2f3e82353f16dce886f3e526f9c598bd8fdc814
mojtaba <mojtaba.kh.sh@gmail.com>
2021-02-22 16:38:42 +0330

    install apollo and configure it

---
+++ resources/js/apollo.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import ApolloClient from 'apollo-boost'
+import VueApollo from 'vue-apollo'
+
+Vue.use(VueApollo)
+
+const apolloClient = new ApolloClient({
+    uri: 'http://localhost:8000/graphql'
+})
+
+export const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+})
+
+
+
+
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,13 @@
 import './bootstrap';
 import Vue from 'vue';
-import Board from './view/Board'
+import Board from './view/Board';
+import apolloProvider from './apollo'
+
+window.Vue = Vue;
 
 const app = new Vue({
     el: '#app',
+    apolloProvider,
     components:{
         Board,
     },
