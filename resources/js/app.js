bda1b12586fdb5f94d60872b742eb413948c085c
mojtaba <mojtaba.kh.sh@gmail.com>
2021-02-22 17:46:25 +0330

    install vue-router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,15 @@
 import './bootstrap';
 import Vue from 'vue';
 import Board from './view/Board';
-import apolloProvider from './apollo'
+import {apolloProvider} from './apollo';
+import {router} from './routes';
 
 window.Vue = Vue;
 
 const app = new Vue({
     el: '#app',
     apolloProvider,
+    router,
     components:{
         Board,
     },
---
+++ resources/js/routes.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+    { path: '/foo', component: Foo },
+    { path: '/bar', component: Bar }
+];
+
+export const router = new VueRouter({
+    routes
+});
