aa2a2622b398a1c4ce9dfd1f35a93399ae1d12a9
mojtaba <mojtaba.kh.sh@gmail.com>
2021-02-27 18:01:00 +0330

    modify web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{all?}', function () {
-    return view('welcome');
-})->where('all','^(?!api\/)[\/\w\.\,-]*');
+Route::fallback(function () {
+    return view('index');
+});
